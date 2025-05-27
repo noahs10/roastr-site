81d3a34cfd81e542f9cbbbd216f91fa21bf92ea1
Noah Syanahan <noahsyanahan@gmail.com>
2025-05-28 00:57:13 +0700

    change font to Jakarta Plus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,6 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "roastr - Explore Indo's Coffees and Roasters",
