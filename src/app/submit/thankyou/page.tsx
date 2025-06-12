9891b18d02331c12e2899c29b617d6c57dcef12f
Noah Syanahan <noahsyanahan@gmail.com>
2025-06-12 23:14:17 +0700

    fix back to homepage button at thank you page

--- src/app/submit/thankyou/page.tsx
+++ src/app/submit/thankyou/page.tsx
@@ -1,15 +1,16 @@
-import { Link } from "lucide-react";
+import Link from "next/link";
 
 export default function ThankYouPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center px-4 text-center">
       <h1 className="text-3xl font-bold mb-4">🎉 Thank You!</h1>
       <p className="text-lg mb-6">We’ve received your brew log submission. We’ll be in touch soon.</p>
-      <p className="text-lg mb-6">the team will do every</p>
+
       <Link
         href="/"
-        className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800 transition-colors"
+        className="bg-white border border-black hover:bg-black hover:text-white px-2 py-2 rounded-md text-sm font-semibold transition-colors inline-block"
       >
+    
         ← Back to Home
       </Link>
     </div>
